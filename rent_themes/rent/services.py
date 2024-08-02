7414021cc89025a56dbe1886dbc788dfded6831a
rommuloifrn <roomulo.cavalkant@outlook.com>
2024-08-02 11:03:08 +0000

    wip: implementando lógica de desconto (código limpado e funcionando como antes)

--- rent_themes/rent/services.py
+++ rent_themes/rent/services.py
@@ -5,7 +5,8 @@ import calendar
 
 class RentServices():
     def salvarRent(self, request):
-        comprou_antes = False
+        cliente_comprou_antes = False
+        
         a = Address(street = request.POST['street'],
                  number = request.POST['number'],
                  complement = request.POST['complement'], 
@@ -20,14 +21,20 @@ class RentServices():
                  client_id= request.POST['select_client'],
                  theme_id = request.POST['select_theme'],
                  address = a )
-        given_date = datetime.datetime.strptime(r.date, "%Y-%m-%d")
-        if r.date == given_date.isoweekday(5) or r.date == given_date.isoweekday(6) or r.date == given_date.isoweekday(7):
-            price = price - (price * 0.4)
+
+        # negocio: 
+        # caso o cliente já tiver comprado antes, dar um desconto de 10%.
+        # caso o aluguel seja feito para uma segunda ou quinta feira, recebe um desconto de 40%.
+        date_em_datetime = datetime.datetime.strptime(r.date, "%Y-%m-%d")
+        #if r.date == given_date.isoweekday(5) or r.date == given_date.isoweekday(6) or r.date == given_date.isoweekday(7):
+            #price = price - (price * 0.4)
+
         for i in Rent.objects.all():
             if i.client.id == r.client_id:
-                comprou_antes = True
-        if comprou_antes == True:
-            price = price - (price * 0.1)
-        if (comprou_antes == True) and (r.date == given_date.isoweekday(5) or r.date == given_date.isoweekday(6) or r.date == given_date.isoweekday(7)):
-            price = price - (price * 0.5)
+                cliente_comprou_antes = True
+
+        #if comprou_antes == True:
+            #price = price - (price * 0.1)
+        #if (comprou_antes == True) and (r.date == given_date.isoweekday(5) or r.date == given_date.isoweekday(6) or r.date == given_date.isoweekday(7)):
+            #price = price - (price * 0.5)
         return r.save()
