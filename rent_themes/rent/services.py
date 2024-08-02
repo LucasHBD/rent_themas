3f6a6adfd5172da6b4f6ece50c3e1e9196cd3153
LucasHBD <130990709+LucasHBD@users.noreply.github.com>
2024-08-02 14:21:16 +0000

    Projeto funcionalgit status

--- rent_themes/rent/services.py
+++ rent_themes/rent/services.py
@@ -6,28 +6,13 @@ from django.shortcuts import get_object_or_404
 
 
 class RentServices():
-    def salvarRent(self, request):
+    def salvarRent(self, a, r):
         desconto_de_dia_da_semana = 0
         desconto_de_cliente_previo = 0
-        tema = get_object_or_404(Theme, pk = request.POST['select_theme'])
-        cliente = get_object_or_404(Client, pk = request.POST['select_client'])
+        tema = get_object_or_404(Theme, pk = r.theme.id)
+        cliente = get_object_or_404(Client, pk = r.client.id)
 
-        
-        a = Address(street = request.POST['street'],
-                 number = request.POST['number'],
-                 complement = request.POST['complement'], 
-                 district = request.POST['district'],
-                 city = request.POST['city'],
-                 state = request.POST['state'] )
         a.save()
-        
-        r = Rent(date=request.POST['date'], 
-                 start_hours=request.POST['start_hours'],
-                 end_hours=request.POST['end_hours'],
-                 client_id= request.POST['select_client'],
-                 theme_id = request.POST['select_theme'],
-                 address = a )
-
         # negocio: 
         # caso o cliente já tiver comprado antes, dar um desconto de 10%.
         # caso o aluguel seja feito para dias dentro do intervalo de segunda e quinta feira, recebe um desconto de 40%.
